262e4ce735afb175e6ce3f2cbafa0316206d7682
Thet Naing Aye <mmu1tn@gmail.com>
2025-09-30 13:06:40 -0600

    refactor: MRD with domain values

--- A02tak605.py
+++ A02tak605.py
@@ -1,51 +1,53 @@
 import time
 class Sudoku:
-    def __init__(self, size, values):
-        rank_map = {
-            "4x4" : 2,
-            "9x9" : 3,
-            "16x16" : 4
-        }
-        self.rank = rank_map[size]
+    def __init__(self, rank, state, domains=None):
+        self.rank = rank
         self.size = self.rank**2
-        self.values = values
-        self.board = self._make_board()
-        
-    def reset(self):
-        self.board = self.make_board(self.rank, self.values)
+        self.state = state
+        if domains is None:
+            self.domains = [[[i for i in range(1, self.size+1)] for _ in range(self.size)] for _ in range(self.size)]
+        else:
+            self.domains = domains
 
-    def _make_board(self):
-        values = self.values.split(" ")
-        board = []
-        idx = 0
-        for _ in range(self.size):
-            row = []
-            for _ in range(self.size):
-                row.append(int(values[idx]))
-                idx +=1
-            board.append(row)
-        return board
-
+    def get_state(self):
+        copy_state = []
+        for row in self.state:
+            copy_state.append(list(row))
+        return copy_state
+    
+    def get_domains(self):
+        values = []
+        for row in self.domains:
+            value_row=[]
+            for col in row:
+                value_row.append(list(col))
+            values.append(value_row)
+        return values
+    
+    def deep_copy(self):
+        return Sudoku(self.rank, self.get_state(), self.get_domains())
+    
     def is_complete(self):
         return not self.has_unassinged_cell()
 
     def has_unassinged_cell(self):
         for row in range(self.size):
             for col in range(self.size):
-                if self.board[row][col] == 0:
+                if self.state[row][col] == 0:
                     return True
         return False
     
     def assign_cell(self, cell, value):
         row, col = cell
-        self.board[row][col] = value
+        self.state[row][col] = value
 
     def unassign_cell(self, cell):
         row, col = cell
-        self.board[row][col] = 0
+        self.state[row][col] = 0
 
     def legal_values(self, pos):
-        return [v for v in range(1, self.size+1) if self.is_consistent(pos, v)]
+        r,c = pos
+        return [v for v in self.domains[r][c] if self.is_consistent(pos, v)]
     
     def select_unassigned_cell(self):
         """
@@ -56,7 +58,7 @@ class Sudoku:
         best_lv = []
         for row in range(self.size):
             for col in range(self.size):
-                if self.board[row][col] == 0:
+                if self.state[row][col] == 0:
                     lv = self.legal_values((row, col))
                     if len(lv) < best_len:
                         best = (row, col)
@@ -65,19 +67,11 @@ class Sudoku:
                         if best_len == 1:  # quick exit if only one choice
                             return best, best_lv
         return best, best_lv
-
-
-    # def select_unassigned_cell(self):
-    #     for row in range(self.size):
-    #         for col in range(self.size):
-    #             if self.board[row][col] == 0:
-    #                 return (row,col)
-    #     return None
     
     def print(self):
         for row in range(self.size):
             for col in range(self.size):
-                print(self.board[row][col], end=" ")
+                print(self.state[row][col], end=" ")
             print()
 
     def pprint(self):
@@ -85,23 +79,23 @@ class Sudoku:
         for i in range(self.size):
             print("|", end="")
             for j in range(self.size):
-                value_str = f"{self.board[i][j] or ' '}".rjust(2).center(4)
+                value_str = f"{self.state[i][j] or ' '}".rjust(2).center(4)
                 print(f"{value_str}",end="|")
             print()
             print(f"-----" * self.size)
 
     def get_value(self, pos):
         row, col = pos
-        return self.board[row][col]
+        return self.state[row][col]
 
     def get_row_values_by_pos(self,pos):
         r_idx, c_idx = pos
-        return [ x for x in self.board[r_idx]]
+        return [ x for x in self.state[r_idx]]
     
     def get_col_values_by_pos(self, pos):
         r_idx, c_idx = pos
         values = []
-        for row in self.board:
+        for row in self.state:
             values.append(row[c_idx])
         return values
     
@@ -112,7 +106,7 @@ class Sudoku:
         c_start = c_idx - c_idx % self.rank
         for r in range(r_start, r_start + self.rank):
             for c in range(c_start, c_start + self.rank):
-                values.append(self.board[r][c])
+                values.append(self.state[r][c])
         return values
 
     def is_consistent(self, pos, value):
@@ -130,16 +124,35 @@ class Sudoku:
         
         return True
 
+    def get_neighours(self, pos):
+        r_idx,c_idx = pos
+
+        neighbours = []
+        for i in range(self.size):
+            if i != r_idx: # get same column neighours
+                neighbours.append((i, c_idx)) 
+            if i != c_idx:
+                neighbours.append((r_idx,i))
+        r_start = r_idx - r_idx % self.rank
+        c_start = c_idx - c_idx % self.rank
+        for r in range(r_start, r_start + self.rank):
+            for c in range(c_start, c_start + self.rank):
+                pos = (r,c)
+                if pos not in neighbours:
+                    neighbours.append((r,c))
+        return neighbours
+    
 def recursive_backtracking(board):
     if board.is_complete():
-        return True
+        return board
 
     cell, legal_values = board.select_unassigned_cell() # cell is tuple (row_idx, col_idx)
     
     for value in legal_values:
         board.assign_cell(cell, value)
-        if recursive_backtracking(board):
-            return True # return Success
+        result =  recursive_backtracking(board.deep_copy())
+        if result:
+            return result # return Success
 
         # remove var from assignment
         board.unassign_cell(cell)
@@ -148,10 +161,10 @@ def recursive_backtracking(board):
     return False
 
 def backtracking(board):
-    is_solved = recursive_backtracking(board)
-    if is_solved:
+    solved_board = recursive_backtracking(board)
+    if solved_board:
         print("The solution is")
-        board.print()
+        solved_board.print()
         # board.pprint()
     else:
         print("No solution found!!!") 
@@ -176,11 +189,28 @@ def run():
     0 0 15 0 0 10 0 0 0 5 3 13 1 0 0 4 2 0 0 0 8 7 13 4 10 0 0 6 9 0 14 15 0 0 4 0 5 0 14 0 16 0 1 9 0 0 0 2 8 9 13 0 16 6 0 1 2 14 0 15 0 7 11 0 0 12 0 1 0 5 0 16 0 13 0 11 2 0 0 3 0 0 9 15 0 1 4 6 0 0 0 12 0 13 0 11 16 0 3 0 0 9 0 14 0 0 0 4 0 0 15 0 4 7 8 0 3 12 0 2 0 15 0 0 0 0 6 0 0 5 0 0 0 0 16 0 9 0 15 10 0 2 3 6 0 15 0 0 10 0 0 0 13 0 11 0 0 14 0 1 13 0 12 0 1 0 0 0 3 6 2 0 16 9 0 0 3 0 0 14 6 0 9 0 8 0 5 0 10 0 7 0 0 8 14 0 12 0 1 15 4 0 6 2 0 11 10 16 12 0 0 0 4 16 0 5 0 7 0 8 0 3 0 0 15 13 0 6 11 0 0 10 12 1 16 3 0 0 0 9 11 0 0 4 2 8 3 0 0 0 14 0 0 6 0 0
     """
     print("Welcome to the Sudoku Solver!")
-    size = input("Enter a size (4x4, 9x9, 16x16):\n")
-    values = input("Enter the values (a blank is represented by a 0):\n")
-    board = Sudoku(size, values)
-    # board.pprint()
+    input_size = input("Enter a size (4x4, 9x9, 16x16):\n")
+    input_values = input("Enter the values (a blank is represented by a 0):\n")
+
+
+    rank_map = {
+        "4x4" : 2,
+        "9x9" : 3,
+        "16x16" : 4
+    }
+    values = input_values.split(" ")
+    state = []
+    rank = rank_map[input_size]
+    idx = 0
     
+    for _ in range(rank**2):
+        row = []
+        for _ in range(rank**2):
+            row.append(int(values[idx]))
+            idx +=1
+        state.append(row)
+    board = Sudoku(rank, state, domains=None)
+    # board.pprint()
    
     start = time.perf_counter()
     backtracking(board)
